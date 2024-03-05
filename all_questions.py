cdae656a8c8a5614fe09d52a570d70a98980d2cf
TallyGreen <127121815+TallyGreen@users.noreply.github.com>
2024-03-05 15:26:08 -0500

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -98,13 +98,13 @@ def question3():
     answers = {}
 
     # type: a string that evaluates to a float
-    answers["(a) SSE"] = 4*(R^2)
+    answers["(a) SSE"] = 4*(R**2)
 
     # type: a string that evaluates to a float
-    answers["(b) SSE"] = 4*((a^2)+(b^2)+(R^2)) 
+    answers["(b) SSE"] = 4*((a**2)+(b**2)+(R**2)) 
 
     # type: a string that evaluates to a float
-    answers["(c) SSE"] = 9(R^2)
+    answers["(c) SSE"] = 9(R**2)
 
     return answers
 
